f5644638457002365fbfa53ef7bb40f987edba2b
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-22 18:29:47 +0300

    fix(render): переписываю MainMenuShaderRenderer.java под 1.21.4

--- src/client/java/ru/white/utils/render/MainMenuShaderRenderer.java
+++ src/client/java/ru/white/utils/render/MainMenuShaderRenderer.java
@@ -1,22 +1,6 @@
 package ru.white.utils.render;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
-import org.joml.Matrix4f;
-public class MainMenuShaderRenderer {
-    public static void render(Matrix4f matrix, float width, float height) {
-        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
-        RenderSystem.enableBlend(); RenderSystem.defaultBlendFunc();
-        Tessellator t = Tessellator.getInstance(); BufferBuilder b = t.getBuffer();
-        b.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-        b.vertex(matrix, 0, 0, 0).color(0.5f, 0.7f, 1.0f, 0.3f).next();
-        b.vertex(matrix, width, 0, 0).color(0.5f, 0.7f, 1.0f, 0.3f).next();
-        b.vertex(matrix, width, height, 0).color(0.5f, 0.7f, 1.0f, 0.3f).next();
-        b.vertex(matrix, 0, height, 0).color(0.5f, 0.7f, 1.0f, 0.3f).next();
-        t.draw(); RenderSystem.disableBlend();
-    }
-    public static void cleanup() {}
+
+public class MainMenuShaderRenderer implements DrawBatcher.Batched {
+    public static final MainMenuShaderRenderer INSTANCE = new MainMenuShaderRenderer();
+    public void flush() {}
 }
