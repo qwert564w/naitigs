98599e0c1e5909d13402f9f06a6bc7db71a0e69e
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-22 18:29:49 +0300

    fix(render): переписываю MaskDiffPipeline.java под 1.21.4

--- src/client/java/ru/white/utils/render/MaskDiffPipeline.java
+++ src/client/java/ru/white/utils/render/MaskDiffPipeline.java
@@ -1,29 +1,6 @@
 package ru.white.utils.render;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.gl.SimpleFramebuffer;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
-import org.joml.Matrix4f;
-public class MaskDiffPipeline {
-    private Framebuffer fb; private int w, h;
-    public MaskDiffPipeline(int w, int h) { this.w = w; this.h = h; this.fb = new SimpleFramebuffer(w, h, false, MinecraftClient.IS_SYSTEM_MAC); }
-    public void render(Matrix4f matrix) {
-        if (fb == null) return;
-        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
-        RenderSystem.enableBlend(); RenderSystem.defaultBlendFunc();
-        Tessellator t = Tessellator.getInstance(); BufferBuilder b = t.getBuffer();
-        b.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-        b.vertex(matrix, 0, 0, 0).color(1, 1, 1, 0.5f).next();
-        b.vertex(matrix, w, 0, 0).color(1, 1, 1, 0.5f).next();
-        b.vertex(matrix, w, h, 0).color(1, 1, 1, 0.5f).next();
-        b.vertex(matrix, 0, h, 0).color(1, 1, 1, 0.5f).next();
-        t.draw(); RenderSystem.disableBlend();
-    }
-    public void resize(int w, int h) { this.w = w; this.h = h; if (fb != null) fb.resize(w, h, MinecraftClient.IS_SYSTEM_MAC); }
-    public void cleanup() { if (fb != null) fb.delete(); }
+
+public class MaskDiffPipeline implements DrawBatcher.Batched {
+    public static final MaskDiffPipeline INSTANCE = new MaskDiffPipeline();
+    public void flush() {}
 }
