4464772cfaccc529868e7a4f7e1d3bb58299c2b2
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-08-23 19:53:29 +0300

    fix: AntiDarkness compilation errors (getEffectId, EventTick, onEnable)

--- src/client/java/ru/white/module/impl/render/AntiDarkness.java
+++ src/client/java/ru/white/module/impl/render/AntiDarkness.java
@@ -3,6 +3,7 @@ package ru.white.module.impl.render;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
 import ru.white.manager.event_impl.EventPacket;
+import ru.white.manager.event_impl.EventTick;
 import ru.white.manager.events.orbit.EventHandler;
 import ru.white.module.api.Category;
 import ru.white.module.api.Module;
@@ -21,8 +22,7 @@ public class AntiDarkness extends Module {
 
         if (event.getPacket() instanceof EntityStatusEffectS2CPacket effectPacket) {
             try {
-                var effectType = effectPacket.getEffectType();
-                // ПРАВИЛЬНО для 1.21.11: сравниваем RegistryEntry напрямую
+                var effectType = effectPacket.getEffectId(); // getEffectId() for 1.21+ Yarn mappings
                 if (effectType != null && effectType == StatusEffects.DARKNESS) {
                     event.cancel();
                     return;
@@ -31,23 +31,23 @@ public class AntiDarkness extends Module {
         }
     }
 
-    @Override
-    public void onEnable() {
-        if (mc.player != null && mc.player.hasStatusEffect(StatusEffects.DARKNESS)) {
-            mc.player.removeStatusEffect(StatusEffects.DARKNESS);
-        }
-        if (mc.player != null && mc.player.isOnFire()) {
-            mc.player.setFireTicks(0);
-        }
-    }
-
-    @Override
-    public void onUpdate() {
+    @EventHandler
+    public void onTick(EventTick event) {
         if (!isEnabled() || mc.player == null) return;
         if (mc.player.hasStatusEffect(StatusEffects.DARKNESS)) {
             mc.player.removeStatusEffect(StatusEffects.DARKNESS);
         }
         if (mc.player.isOnFire() && mc.player.getFireTicks() > 0) {
+            mc.player.setFireTicks(0);
+        }
+    }
+
+    @Override
+    protected void onEnable() {
+        if (mc.player != null && mc.player.hasStatusEffect(StatusEffects.DARKNESS)) {
+            mc.player.removeStatusEffect(StatusEffects.DARKNESS);
+        }
+        if (mc.player != null && mc.player.isOnFire()) {
             mc.player.setFireTicks(0);
         }
     }
